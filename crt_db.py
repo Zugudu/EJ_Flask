6a807956963d4b9c690aad07d2a3fe34f0b7c1f6
Zugudu <2cuco2@gmail.com>
2020-12-15 13:02:39 +0200

    refactor
    crt_db refactor sequence

--- crt_db.py
+++ crt_db.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     db = mysql.connector.connect(**db_config.db)
     cursor = db.cursor()
 
-    sql_for_session = '''
+    sql_arr = ['''
 CREATE TABLE `session` (
     `id` INT NOT NULL AUTO_INCREMENT ,
     `id_user` INT NOT NULL ,
@@ -16,27 +16,19 @@ CREATE TABLE `session` (
     `hash` VARCHAR(64) NOT NULL ,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
-    '''
-
-    sql_for_fk = '''
+    ''','''
 ALTER TABLE `session`
 ADD CONSTRAINT `fk_user`
 FOREIGN KEY (`id_user`)
 REFERENCES `dovidnyuk_students`(`id_student`)
 ON DELETE CASCADE ON UPDATE CASCADE;
-    '''
-
-    sql_for_pass_expand = '''
+    ''','''
 ALTER TABLE `dovidnyuk_students`
 CHANGE `password`
 `password` VARCHAR(64) NOT NULL;
-    '''
-
-    sql_for_pass_not_uq = '''
+    ''','''
 ALTER TABLE `dovidnyuk_students` DROP INDEX `password_UNIQUE`;
-    '''
-
-    sql_for_view0 = '''
+    ''','''
 CREATE VIEW `v_students_mark` AS
     SELECT
         `dovidnyuk_student_marks`.`PK_student_mark` AS `PK_student_mark`,
@@ -52,9 +44,7 @@ CREATE VIEW `v_students_mark` AS
         LEFT JOIN `dovidnyuk_zaniat` ON ((`dovidnyuk_student_marks`.`FK_zaniattya` = `dovidnyuk_zaniat`.`PK_zaniattya`)))
         LEFT JOIN `dovidnyuk_group_load` ON ((`dovidnyuk_zaniat`.`FK_load` = `dovidnyuk_group_load`.`PK_load`)))
         LEFT JOIN `dovidnyuk_vudiv_zaniat` ON ((`dovidnyuk_zaniat`.`FK_vudy_zaniattya` = `dovidnyuk_vudiv_zaniat`.`id_vudy_zaniattya`)));
-    '''
-
-    sql_for_view1 = '''
+    ''','''
 CREATE VIEW `v_stundent_lesson` AS
     SELECT
         `dovidnyuk_students`.`id_student` AS `id_student`,
@@ -64,25 +54,19 @@ CREATE VIEW `v_stundent_lesson` AS
         ((`dovidnyuk_group_load`
         JOIN `dovidnyuk_students` ON ((`dovidnyuk_students`.`fk_group` = `dovidnyuk_group_load`.`FK_group`)))
         JOIN `dovidnyuk_discipline` ON ((`dovidnyuk_discipline`.`PK_discipline` = `dovidnyuk_group_load`.`FK_discipline`)));
-    '''
+    ''']
+
+    msg_arr = ["Session was added", "FK for session was added",
+    "Pass was expanded", "Pass was made not unique",
+    "View 0 created", "View 1 created"]
 
     print("Starting...")
-    cursor.execute(sql_for_session)
-    db.commit()
-    print("Session was added")
-    cursor.execute(sql_for_fk)
-    db.commit()
-    print("FK for session was added")
-    cursor.execute(sql_for_pass_expand)
-    db.commit()
-    print("Pass was expanded")
-    cursor.execute(sql_for_pass_not_uq)
-    db.commit()
-    print("Pass was made not unique")
-    cursor.execute(sql_for_view0)
-    db.commit()
-    print("View 0 created")
-    cursor.execute(sql_for_view1)
-    db.commit()
-    print("View 1 created")
+
+    try:
+        for i in range(0, len(sql_arr)):
+                cursor.execute(sql_arr[i])
+                print(msg_arr[i], '[{}/{}]'.format(i+1, len(sql_arr)))
+                db.commit()
+    except mysql.connector.errors.ProgrammingError:
+        print('DB are already created')
     db.close()
